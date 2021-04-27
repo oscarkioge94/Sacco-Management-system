ca6e49b2bb1e83747ba6de0c63f43436a5f03735
unknown <mainaoscar94@gmail.com>
2021-04-27 15:17:49 +0300

    ddone with register functionality and index.html

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD7-H9noXwqy6D2ocXKRjWDpiITnghxzro",
+    authDomain: "sacco-management-c5fbb.firebaseapp.com",
+    projectId: "sacco-management-c5fbb",
+    storageBucket: "sacco-management-c5fbb.appspot.com",
+    messagingSenderId: "959308986168",
+    appId: "1:959308986168:web:bf9202e77eae256edc73cd",
+    measurementId: "G-C0CWZZZGP5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
---
+++ js/register.js
@@ -0,0 +1,43 @@
+document.getElementById('signUp').onclick = ()=>{
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var password1 = document.getElementById('password1').value;
+    var password2 = document.getElementById('password2').value;
+    var role = document.getElementById('role').value;
+    
+
+    // signup logic
+    if(password1 ==  password2){
+      firebase.auth().createUserWithEmailAndPassword(email, password1)
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+
+    firebase.firestore().collection("users").add({
+      Name: name,
+      Email: email,
+      Password: password1,
+      UserId: user.uid,
+      Role: role
+  })
+      .then((docRef) => {
+          console.log("Document written with ID: ", docRef.id);
+      })
+      .catch((error) => {
+          console.error("Error adding document: ", error);
+      })
+      .then(()=>{
+        window.location.href="../login.html";
+      })
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+  });
+    }
+
+   
+
+}
